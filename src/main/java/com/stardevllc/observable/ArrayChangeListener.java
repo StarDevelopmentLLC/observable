123f9d45f00bce4ff2e42da7d86d73769e65c9bc
Firestar311 <slaterandrew0398@gmail.com>
2024-10-16 15:08:12 -0500

    Applied changes from the eventbus library

--- src/main/java/com/stardevllc/observable/ArrayChangeListener.java
+++ src/main/java/com/stardevllc/observable/ArrayChangeListener.java
@@ -1,11 +1,10 @@
 package com.stardevllc.observable;
 
-import com.stardevllc.eventbus.Event;
 import com.stardevllc.eventbus.SubscribeEvent;
 
 @SubscribeEvent
 @FunctionalInterface
 public interface ArrayChangeListener<T> {
     void changed(ArrayChangeEvent<T> event);
-    record ArrayChangeEvent<T>(ObservableArray<T> array, int index, T oldValue, T newValue) implements Event {}
+    record ArrayChangeEvent<T>(ObservableArray<T> array, int index, T oldValue, T newValue) {}
 }
--- src/main/java/com/stardevllc/observable/ChangeListener.java
+++ src/main/java/com/stardevllc/observable/ChangeListener.java
@@ -1,11 +1,10 @@
 package com.stardevllc.observable;
 
-import com.stardevllc.eventbus.Event;
 import com.stardevllc.eventbus.SubscribeEvent;
 
 @SubscribeEvent
 @FunctionalInterface
 public interface ChangeListener<T> {
     void changed(ChangeEvent<T> event);
-    record ChangeEvent<T>(ObservableValue<? extends T> observableValue, T oldValue, T newValue) implements Event {}
+    record ChangeEvent<T>(ObservableValue<? extends T> observableValue, T oldValue, T newValue) {}
 }
